f2a592dc1587810e1f1b7804404c5613d93d21a2
Olga Kuptsova <helllga2018@yandex.ru>
2023-03-13 16:41:25 +0400

    error fixed

--- src/pages/chat/chat.js
+++ src/pages/chat/chat.js
@@ -1,6 +1,7 @@
 import Handlebars from 'handlebars';
 import chat from 'bundle-text:./chat.hbs';
 import {ChatItem} from '../../components/chat_item/chat_item';
+import {Link} from '../../components/link/link';
 import './chat.css'; 
 
 export const Chat = (data)=> {
@@ -8,5 +9,6 @@ export const Chat = (data)=> {
     data.chatItems.forEach(element => {         
         data.chat_items.push(ChatItem(element));
     });
+    data.link = Link({text:"Profile >", url: "/profile", class_: "grey-text"});
     return Handlebars.compile(chat)(data); 
 }
--- src/pages/error/error.js
+++ src/pages/error/error.js
@@ -1,5 +1,14 @@
 import Handlebars from 'handlebars';
 import error from 'bundle-text:./error.hbs';
+import {Link} from '../../components/link/link';
 import './error.css'; 
 
-export const Error = ({code})=> { return Handlebars.compile(error)({code}); };
+export const Error = (data)=> { 
+    if(data.code==404){
+        data.message = "Sorry, this page doesn't exist";
+    }else if(data.code==500){
+        data.message = "We are fixing the problem";
+    }
+    data.link = Link({text:"Back to chats", url: "/chat"})
+    return Handlebars.compile(error)(data); 
+};
